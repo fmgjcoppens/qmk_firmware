651c2f608fc8e4c2373eb09d61c537ab2160d455
François Coppens <mail@fmgjcoppens.eu>
2024-10-16 15:29:06 +0200

    Switched Page up/down and Home keys

--- keyboards/gmmk/pro/rev1/ansi/keymaps/fmgjcoppens/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/fmgjcoppens/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 {
     [_BL] = LAYOUT(
         KC_ESC,     KC_F1,      KC_F2,      KC_F3,  KC_F4,          KC_F5,      KC_F6,      KC_F7,          KC_F8,          KC_F9,          KC_F10,     KC_F11,     KC_F12,     KC_DEL,                 KC_MUTE,
-        KC_GRV,     KC_1,       KC_2,       KC_3,   KC_4,           KC_5,       KC_6,       KC_7,           KC_8,           KC_9,           KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                KC_HOME,
-        KC_TAB,     KC_Q,       KC_W,       KC_E,   KC_R,           KC_T,       KC_Y,       KC_U,           KC_I,           KC_O,           KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                KC_PGUP,
-        KC_CAPS,    KC_A,       KC_S,       KC_D,   LT(_AL,KC_F),   KC_G,       KC_H,       LT(_GL,KC_J),   LT(_PL,KC_K),   LT(_TL,KC_L),   KC_SCLN,    KC_QUOT,                KC_ENT,                 KC_PGDN,
+        KC_GRV,     KC_1,       KC_2,       KC_3,   KC_4,           KC_5,       KC_6,       KC_7,           KC_8,           KC_9,           KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                KC_PGUP,
+        KC_TAB,     KC_Q,       KC_W,       KC_E,   KC_R,           KC_T,       KC_Y,       KC_U,           KC_I,           KC_O,           KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                KC_PGDN,
+        KC_CAPS,    KC_A,       KC_S,       KC_D,   LT(_AL,KC_F),   KC_G,       KC_H,       LT(_GL,KC_J),   LT(_PL,KC_K),   LT(_TL,KC_L),   KC_SCLN,    KC_QUOT,                KC_ENT,                 KC_HOME,
         KC_LSFT,                KC_Z,       KC_X,   KC_C,           KC_V,       KC_B,       KC_N,           KC_M,           KC_COMM,        KC_DOT,     KC_SLSH,                KC_RSFT,    KC_UP,      KC_END,
         KC_LCTL,    KC_LALT,    KC_LGUI,                                        KC_SPC,                                                     MO(_CL),    KC_RALT,    KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT
     ),
@@ -313,7 +313,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max)
  * after this state has been restored, effectively overwriting
  * the last known state every time the keyboard boots, and
  * replacing it with the state described here.
- * 
+ *
  * I suppose that, normally, you don't want this.
  */
 // void keyboard_post_init_user(void) {
